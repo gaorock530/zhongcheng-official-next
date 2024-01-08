7f1a66e8cfbe67ccfffcda1cf5d93729ceeb3842
gaorock530 <gaorock530@hotmail.com>
2024-01-08 11:33:33 +0800

    sass

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,38 @@
 // import Image from 'next/image'
-import styles from './Home.module.css'
+import './home.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
 
-    <main className={styles.main}>
-      <h1 className={styles.title}>
+    <main className="main">
+      <h1 className="title">
         <img src="/logo_logo.png"/>
         众城保障房
       </h1>
 
-      <div className={styles.grid}>
-        <a className={styles.card}>
-          <img src="/pic2.jpg" className={styles.image}/>
+      <div className="grid">
+        <a className="card">
+          <img src="/pic2.jpg" className="image"/>
           <p>西苑丽景小区</p>
         </a>
 
-        <a className={styles.card}>
-          <img src="/pic6.jpg" className={styles.image}/>
+        <a className="card">
+          <img src="/pic6.jpg" className="image"/>
           <p>荣源小区</p>
         </a>
-        <a className={styles.card}>
-          <img src="/pic4.jpg"  className={styles.image}/>
+        <a className="card">
+          <img src="/pic4.jpg"  className="image"/>
           <p>乔谢小区</p>
         </a>
-        <a className={styles.card}>
-          <img src="/pic5.jpg"  className={styles.image}/>
+        <a className="card">
+          <img src="/pic5.jpg"  className="image"/>
           <p>新牧湾小区</p>
         </a>
       </div>
     </main>
 
-    <footer className={styles.footer}>
+    <footer className="footer">
       <p>新乡市众城保障性住房经营管理有限公司 © 2024 版权所有</p>
       <p><a
         href="https://beian.miit.gov.cn"
